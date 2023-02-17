82cceb79f3de3f270b302183ecabe37581227869
ranamohamedd <ranamohamedd0@gmail.com>
2023-02-17 12:32:30 -0800

    1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
 /**
@@ -20,5 +22,5 @@ int main(void)
 		printf("and is 0\n");
 	else if (n % 10 < 6 && n % 10 != 0)
 		printf("and is less than 6 and not 0\n");
-}	return (0);
-
+	return (0);
+}
